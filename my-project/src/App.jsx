351e05d5a294b3ce55cbeded3bc661338a754f29
MikeWeDev <155253264+MikeWeDev@users.noreply.github.com>
2024-02-06 14:18:22 -0300

    the version that works routing

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,19 +2,27 @@ import './index.css'
 import Home from './pages/Home'
 import { BrowserRouter as Router, Routes ,Route } from "react-router-dom"
 import ProductList from './pages/ProductList'
-import Product from './pages/Product'
+//import Product from './pages/Product'
 import Rigister from './pages/Rigister'
 import Login from './pages/Login'
 import Cart from './pages/Cart'
 function App() {
 
   return (
-    //<ProductList />
-     <Home /> 
-     // <Product />
-    // <Rigister />
-    // <Login />
-   // <Cart />
+   <>
+    <Router>
+      <Routes>
+       <Route index path='/' element={<Home  /> } />
+        <Route  path='/c' element={<Cart />} />
+        <Route path='/r' element={<Rigister />} />
+        <Route path='/lo' element={<Login />} />
+        <Route path='/p' element={<ProductList/>} />
+      </Routes>
+    </Router>
+    
+  
+   </>
+  
   )
 }
 
