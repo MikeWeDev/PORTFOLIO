49285317bf537cdcde74412826e3942c0a58d2c2
MikeWeDev <155253264+MikeWeDev@users.noreply.github.com>
2024-03-15 15:19:59 -0300

    final version 95%

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,7 +1,7 @@
 import Layout from './components/Layout'
 import Home from './components/Home/Home'
 import About from './components/About/About'
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
+import { HashRouter as Router,Route,Routes } from 'react-router-dom'
 import './index.scss'
 import Contact from './components/Contact/Contact'
 import Portfolio from './components/Portfolio/Portfolio-box'
